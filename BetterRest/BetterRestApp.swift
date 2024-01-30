54c5c80cb50ada56d3ab936d298ba88acc8cd8f6
rockgard3n <liamcashel@gmail.com>
2024-01-30 08:26:16 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Liam Cashel on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
